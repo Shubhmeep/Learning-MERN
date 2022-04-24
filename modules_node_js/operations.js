d0ac67054961734ba14f0df993250cb0d579d3df
Shubh Sehgal <shubh.sehgal2506@gmail.com>
2022-04-24 18:45:03 +0530

    first node lecture

---
+++ modules_node_js/operations.js
@@ -0,0 +1,8 @@
+//module = set of re-usable functions which can be accessed outside this file 
+exports.add = function(a,b){
+    return a+b;
+}
+
+exports.multiplication = function(a,b){
+    return a*b;
+}
