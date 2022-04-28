a7d6c769f8fa99d8243f88fecad3473366ccb8f2
Shubh Sehgal <shubh.sehgal2506@gmail.com>
2022-04-28 21:28:30 +0530

    node basics finish

--- basichttpserver/server.js
+++ basichttpserver/server.js
@@ -3,14 +3,56 @@
 
 const http = require('http');
 const port = 8000;
+const fs = require('fs'); // built in module in node js used for reading and writting in files
+//fs is basically used for file specific operations
 
 
-//creating a request handler
+
+// //creating a request handler
+// function requestHandler(request,response){
+//     response.writeHead(200,{'content-type':'text/html'});
+//     fs.readFile('./index.html',function(err,data)
+//     {
+//         if (err){
+//             console.log(err);
+//             return response.end("<h1>ERRROR!!!</h1>");
+//         }
+
+//         return response.end(data);
+
+//     }
+//     );   
+//     //readFile() is Asynchronous and readFileSync() is synchronous
+// }
+
 function requestHandler(request,response){
+        response.writeHead(200,{'content-type':'text/html'});
+        let filepath;
+        switch(request.url){
+            case '/':
+                filepath = './index.html';
+                break;
 
-    response.end('Response Generated !') // whenever server is started this response will be generated
+            case '/profile':
+                filepath = './profile.html';
+                break;
+            
+            default:
+                filepath = './err.html';
+        }
 
-}
+        fs.readFile(filepath,function(err,data){
+            if (err){
+                            console.log(err);
+                            return response.end("<h1>ERRROR!!!</h1>");
+                        }
+            return response.end(data);
+
+        });
+
+        //readFile() is Asynchronous and readFileSync() is synchronous
+    }
+
 
 
 
